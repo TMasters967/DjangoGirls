489f732b2e30465bca22bcd9b02467a96c9dfe12
TMasters967 <tmasters3@ivytech.edu>
2023-10-05 10:12:31 -0400

    added icons -tlm

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
                                CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def published(self):
